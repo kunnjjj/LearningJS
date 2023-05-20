16168df0c5e27aaa39cd451b6a813a9ece7802fa
Kunj Arora <kunj57932@gmail.com>
2023-05-20 11:47:06 +0530

    window object, JS is loosely typed

---
+++ WindowObject/script.js
@@ -0,0 +1,15 @@
+console.log(window)
+// window is a global object, which is created when a js code runs, created by browser
+// this is a keyword which initially points to the window object(at global level)
+
+
+var x=10;
+console.log(x);
+
+console.log(window.x);
+console.log(this.x);
+
+// JS is loosely  typed language, one variable initially may contain integer,then later string
+
+// doing a=undefined is a bad practice (justify  to yourself)
+
